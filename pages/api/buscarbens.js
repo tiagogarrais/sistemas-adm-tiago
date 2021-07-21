bc821c9bbd5fe5f02dd6340ca81c20084f40d603
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2021-07-21 20:01:17 -0300

    Update buscarbens.js

--- pages/api/buscarbens.js
+++ pages/api/buscarbens.js
@@ -1,9 +1,9 @@
 async function buscarbens(request, response) {
 
     const bensUfca = await fetch("http://dadosabertosapi.ufca.edu.br/service/recurso/bens_moveis.json")
-    const bensUfcaJson = await bensUfca.json()
+    //const bensUfcaJson = await bensUfca.json()
     
-    response.json(bensUfcaJson)
+    response.json(bensUfca)
 }
 
 export default buscarbens
