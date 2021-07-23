dc0eb4972811c58ef57a46ff9ccab58e443cd581
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2021-07-22 22:28:22 -0300

    Importar JSON bens_moveis
    Por restrições de uso da versão gratuita da "serveless function" da vercel (ela permite processamentos de até 5 segundos) vou fazer esse teste importando o arquivo bens_moveis.json diretamente para a estrutura do site.  Neste caso vou perder a funcionalidade de manter sempre atualizado mas também não vou depender da disponibilidade de um site externo para fornecer o arquivo atualizado.

--- pages/api/buscarbens.js
+++ pages/api/buscarbens.js
@@ -1,11 +1,11 @@
-import bensUfcaJson from '../../src/assets/json/bensUfca.json'
+import bensUfca from '../../src/assets/json/bens_moveis.json'
 
 function buscarbens(request, response) {
 
     // const bensUfca = await fetch("http://dadosabertosapi.ufca.edu.br/service/recurso/bens_moveis.json")
     //const bensUfcaJson = await bensUfca.json()
     
-    response.json(bensUfcaJson)
+    response.json(bensUfca)
 }
 
 export default buscarbens
