3062ef33072994b11c9117626d1f7d20e7abc992
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2023-04-19 18:07:17 -0300

    Atualizando dependências

--- pages/api/cadastro/buscarCadastro.js
+++ pages/api/cadastro/buscarCadastro.js
@@ -1,8 +1,10 @@
-import connect from "../../../src/utils/mongodb"
+import connect from '../../../src/utils/mongodb'
 
 export default async function buscarCadastro(require, response) {
-    const { db } = await connect()
-    const dados = await db.collection('usuarios').findOne({email:'tiagoarraisholanda@gmail.com'})
-    response.setHeader('Cache-Control', 's-maxage=15, stale-while-revalidate')
-    response.status(200).json(dados)
+  const { db } = await connect()
+  const dados = await db
+    .collection('usuarios')
+    .findOne({ email: 'tiagoarraisholanda@gmail.com' })
+  response.setHeader('Cache-Control', 's-maxage=15, stale-while-revalidate')
+  response.status(200).json(dados)
 }
--- pages/cadastro/Cadastro.js
+++ pages/cadastro/Cadastro.js
@@ -1,38 +1,39 @@
 import axios from 'axios'
-import { useSession, signIn } from "next-auth/react"
-import { useState } from "react"
-
+import { useSession, signIn } from 'next-auth/react'
+import { useState } from 'react'
 
 export default function Cadastro() {
-
   const { data: session } = useSession()
   const [cadastro, setCadastro] = useState({})
+  const [nomeCadastrado, setNomeCadastrado] = useState('')
 
   async function buscarCadastro() {
     const response = await fetch('/api/cadastro/buscarCadastro')
     const data = await response.json()
-    setCadastro(data)
+    setNomeCadastrado(data.nomeCompleto)
   }
 
   function onInputChange(evt) {
-    setCadastro(prevState => ({ ...prevState, [evt.target.id]: evt.target.value }));
+    setCadastro(prevState => ({
+      ...prevState,
+      [evt.target.id]: evt.target.value
+    }))
   }
 
   function btnSaveClick() {
-    axios.post('/api/cadastro/enviarCadastro',
-      {
-        'nomeCompleto': cadastro.nome,
-        'email': cadastro.email,
-        'dataInformacao': Date()
+    axios
+      .post('/api/cadastro/enviarCadastro', {
+        nomeCompleto: cadastro.nome,
+        email: cadastro.email,
+        dataInformacao: Date()
       })
       .then(function (response) {
-        console.log("Cadastro atualizado")
+        console.log('Cadastro atualizado')
       })
       .catch(function (error) {
-        console.log(error);
+        console.log(error)
       })
   }
-
 
   if (session) {
     cadastro.email = session.user.email
@@ -40,43 +41,33 @@ export default function Cadastro() {
     return (
       <div className="conteudo">
         <button onClick={buscarCadastro}>Mostrar meus dados</button>
-        <p>Nome: {cadastro.nome}</p>
+        <p>Nome: {nomeCadastrado}</p>
         <p>E-mail: {cadastro.email}</p>
 
-
-
-        <h2>Meus dados</h2>
-        <label for="nome">Nome completo
+        <h2>Atualizar dados</h2>
+        <label for="nome">
+          Nome completo
           <input
             type="text"
-            placeholder="Seu nome completo"
+            placeholder={nomeCadastrado}
             id="nome"
             value={cadastro.nome}
             onChange={onInputChange}
           />
         </label>
 
-        <label for="email">E-mail
-          <input
-            type="text"
-            id="email"
-            value={cadastro.email}
-            readOnly
-          />
+        <label for="email">
+          E-mail
+          <input type="text" id="email" value={cadastro.email} readOnly />
         </label>
 
-        <input
-          type="button"
-          value='Salvar'
-          onClick={btnSaveClick}
-        />
-
+        <input type="button" value="Salvar" onClick={btnSaveClick} />
       </div>
     )
   }
   return (
     <>
-      <div className='conteudo'>
+      <div className="conteudo">
         <p>Para acessar este conteúdo é necessário fazer login</p>
         <button onClick={() => signIn()}>Entrar</button>
       </div>
