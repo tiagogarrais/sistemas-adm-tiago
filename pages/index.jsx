f8f03ddebaf579b350c7ea47227bf1386fe53e12
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2023-05-14 23:59:35 -0300

    Melhorias visuais na página inicial
    Melhorias visuais com a adição de cards com fotos e informações sobre os sistemas hospedados neste site.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,10 @@
 import { useSession } from 'next-auth/react'
 import Contador from '../components/contador-regressivo/Contador'
 import Link from 'next/link'
+import ambientes from '../public/images/ambientes.jpg'
+import brainstorming from '../public/images/brainstorming.jpg'
+import transportes from '../public/images/transportes.jpg'
+import Image from 'next/image'
 
 export default function Home() {
   const { data: session } = useSession()
@@ -13,25 +17,55 @@ export default function Home() {
           <div className="button">
             <Link href="/cadastro/cadastro">Meu cadastro</Link>
           </div>
-
           <h2>Sistemas desenvolvidos para a UFCA - neste site</h2>
 
-          <div className="button">
-            <Link className="link" href="/ambientes/1">
-              Ambientes IFE
-            </Link>
-          </div>
+          <div class="container my-12 mx-auto px-4 md:px-12">
+            <div class="flex flex-wrap -mx-1 lg:-mx-4">
+              <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <Link className="link" href="/ambientes/1">
+                  <article>
+                    <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                      <p className="text-lg">Ambientes</p>
+                    </header>
+                    <Image
+                      alt="Placeholder"
+                      class="block h-auto w-full"
+                      src={ambientes}
+                    />
+                  </article>
+                </Link>
+              </div>
 
-          <div className="button">
-            <Link className="link" href="/brainstorming/brainstorming">
-              Brainstorming
-            </Link>
-          </div>
+              <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <Link className="link" href="/brainstorming/brainstorming">
+                  <article>
+                    <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                      <p className="text-lg">Brainstorming</p>
+                    </header>
+                    <Image
+                      alt="brainstorming campus Brejo Santo"
+                      class="block h-auto w-full"
+                      src={brainstorming}
+                    />
+                  </article>
+                </Link>
+              </div>
 
-          <div className="button">
-            <Link className="link" href="/transportes/">
-              Transportes IFE
-            </Link>
+              <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <Link className="link" href="/transportes/">
+                  <article>
+                    <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                      <p className="text-lg">Transportes</p>
+                    </header>
+                    <Image
+                      alt="transportes campus Brejo Santo"
+                      class="block h-auto w-full"
+                      src={transportes}
+                    />
+                  </article>
+                </Link>
+              </div>
+            </div>
           </div>
 
           <h2>Sistemas desenvolvidos para a UFCA - em outros sites</h2>
