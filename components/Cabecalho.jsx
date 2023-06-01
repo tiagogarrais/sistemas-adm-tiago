f1a8adb1910aab4af75c98cf888de174cefe30b3
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2023-05-31 23:51:56 -0300

    index sem senha
    A página inicial recebeu algumas atualizações e agora pode ser acessada sem a necessidade de se fazer login.

--- components/Cabecalho.jsx
+++ components/Cabecalho.jsx
@@ -1,12 +1,14 @@
 import Link from 'next/dist/client/link'
-import { useSession, signOut } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
 
 export default function Cabecalho() {
   const { data: session } = useSession()
 
   return (
     <div>
-      <h1 id="titulo">www.admtiago.com.br</h1>
+      <Link href={'/'}>
+        <h1 id="titulo">www.admtiago.com.br</h1>
+      </Link>
       <div>
         <article>
           Respeito - Responsabilidade - Inovação - Transparência
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import { useSession } from 'next-auth/react'
-import Contador from '../components/contador-regressivo/Contador'
 import Link from 'next/link'
 import ambientes from '../public/images/ambientes.jpg'
 import brainstorming from '../public/images/brainstorming.jpg'
@@ -9,67 +8,67 @@ import Image from 'next/image'
 export default function Home() {
   const { data: session } = useSession()
 
-  if (session) {
-    return (
+  return (
+    <div>
+      {session ? sessionStorage.setItem('email', session.user.email) : ''}
       <div>
-        {sessionStorage.setItem('email', session.user.email)}
-        <div>
-          <div className="button">
-            <Link href="/cadastro/cadastro">Meu cadastro</Link>
-          </div>
-          <h2>Sistemas desenvolvidos para a UFCA - neste site</h2>
+        <div className="button">
+          <Link href="/cadastro/cadastro">Meu cadastro</Link>
+        </div>
+        <h2>Sistemas desenvolvidos para a UFCA - neste site</h2>
 
-          <div class="container my-12 mx-auto px-4 md:px-12">
-            <div class="flex flex-wrap -mx-1 lg:-mx-4">
-              <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-                <Link className="link" href="/ambientes/1">
-                  <article>
-                    <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-                      <p className="text-lg">Ambientes</p>
-                    </header>
-                    <Image
-                      alt="Placeholder"
-                      class="block h-auto w-full"
-                      src={ambientes}
-                    />
-                  </article>
-                </Link>
-              </div>
+        <div class="container my-12 mx-auto px-4 md:px-12">
+          <div class="flex flex-wrap -mx-1 lg:-mx-4">
+            <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+              <Link className="link" href="/ambientes/1">
+                <article>
+                  <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                    <p className="text-lg">Ambientes</p>
+                  </header>
+                  <Image
+                    alt="Placeholder"
+                    class="block h-auto w-full"
+                    src={ambientes}
+                  />
+                </article>
+              </Link>
+            </div>
 
-              <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-                <Link className="link" href="/brainstorming/brainstorming">
-                  <article>
-                    <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-                      <p className="text-lg">Brainstorming</p>
-                    </header>
-                    <Image
-                      alt="brainstorming campus Brejo Santo"
-                      class="block h-auto w-full"
-                      src={brainstorming}
-                    />
-                  </article>
-                </Link>
-              </div>
+            <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+              <Link className="link" href="/brainstorming/brainstorming">
+                <article>
+                  <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                    <p className="text-lg">Brainstorming</p>
+                  </header>
+                  <Image
+                    alt="brainstorming campus Brejo Santo"
+                    class="block h-auto w-full"
+                    src={brainstorming}
+                  />
+                </article>
+              </Link>
+            </div>
 
-              <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-                <Link className="link" href="/transportes/">
-                  <article>
-                    <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-                      <p className="text-lg">Transportes</p>
-                    </header>
-                    <Image
-                      alt="transportes campus Brejo Santo"
-                      class="block h-auto w-full"
-                      src={transportes}
-                    />
-                  </article>
-                </Link>
-              </div>
+            <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+              <Link className="link" href="/transportes/">
+                <article>
+                  <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                    <p className="text-lg">Transportes (Em desenvolvimento)</p>
+                  </header>
+                  <Image
+                    alt="transportes campus Brejo Santo"
+                    class="block h-auto w-full"
+                    src={transportes}
+                  />
+                </article>
+              </Link>
             </div>
           </div>
+        </div>
 
-          <h2>Sistemas desenvolvidos para a UFCA - em outros sites</h2>
+        <h2>Sistemas desenvolvidos para a UFCA - em outros sites</h2>
 
+        <div class="container my-12 mx-auto px-4 md:px-12">
           <div className="button">
             <a href="https://ifeadm.16mb.com" target="_blank">
               Site IFE-ADM
@@ -136,27 +135,27 @@ export default function Home() {
             </a>
           </div>
 
-          <h2>Redes Sociais Pessoais</h2>
+          <h2>Criações e Hobbies</h2>
           <div className="button">
-            <a href="https://tiktok.com/@tiagoarraisdeholanda" target="_blank">
-              TikTok
+            <a
+              href="https://www.tiktok.com/@tiagoarraisdeholanda/video/7223301127473827078"
+              target="_blank"
+            >
+              Como fazer o Pão da Stella Maris?
+            </a>
+          </div>
+
+          <div className="button">
+            <a
+              href="https://processos.ifeadm.16mb.com/tiago-processos/como-verificar-se-o-dea-desfiblilador-externo-automatico-esta-funcionando-adequadamente/"
+              target="_blank"
+            >
+              Como verificar se o Desfibrilador Externo Automático - DEA está
+              funcionando adequadamente?
             </a>
           </div>
         </div>
       </div>
-    )
-  }
-  return (
-    <div>
-      <div id="timer">
-        <h2>
-          <Contador />
-        </h2>
-      </div>
-      <p>
-        Aguarde a contagem regressiva, se acabar é necessário fazer login no
-        site.
-      </p>
     </div>
   )
 }
