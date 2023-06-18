4d49e1f3d184d76b33a4fdf70d19aa7ff659d67b
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2023-06-18 02:58:51 -0300

    Update proximas-viagens.jsx
    Ajustando a lógica do componente para que mostre apenas as viagens agendadas para as próximas datas.

--- pages/api/transportes/proximas-viagens.jsx
+++ pages/api/transportes/proximas-viagens.jsx
@@ -2,11 +2,17 @@ import connect from '../../../src/utils/mongodb'
 
 export default async function proximasViagens(require, response) {
   const { db } = await connect()
+  const dataAtual = new Date()
+  const dia = dataAtual.getDate()
+  const mes = dataAtual.getMonth()
+  const ano = dataAtual.getFullYear()
+  const stringData = ano + '-' + mes + '-' + dia
+
   const proximasViagens = await db
     .collection('solicitatransportes')
-    .find()
+    .find({ dataIda: { $gte: new Date(stringData) } })
     .sort({ dataIda: 1 })
     .toArray()
-  response.setHeader('Cache-Control', 's-maxage=15, stale-while-revalidate')
+  //response.setHeader('Cache-Control', 's-maxage=15, stale-while-revalidate')
   response.status(200).json(proximasViagens)
 }
