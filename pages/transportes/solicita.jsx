08934cc7681c68b1e7665b13e11a8a4926db23b5
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2023-06-20 20:46:17 -0300

    Update solicita.jsx

--- pages/transportes/solicita.jsx
+++ pages/transportes/solicita.jsx
@@ -1,8 +1,5 @@
-import { useSession } from 'next-auth/react'
 import SolicitarVeiculo from '../../components/transportes/SolicitarVeiculo'
 
 export default function Solicita() {
-  const { data: session } = useSession()
-
   return <SolicitarVeiculo />
 }
