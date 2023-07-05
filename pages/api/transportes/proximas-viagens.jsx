9b391f8e6eab62aaa0bf9f060ec7c65fcab2e1cb
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2023-07-05 04:33:58 -0300

    Update proximas-viagens.jsx
    Corrigindo erro na geração da data atual

--- pages/api/transportes/proximas-viagens.jsx
+++ pages/api/transportes/proximas-viagens.jsx
@@ -4,7 +4,7 @@ export default async function proximasViagens(require, response) {
   const { db } = await connect()
   const dataAtual = new Date()
   const dia = dataAtual.getDate()
-  const mes = dataAtual.getMonth()
+  const mes = dataAtual.getMonth() + 1
   const ano = dataAtual.getFullYear()
   const stringData = ano + '-' + mes + '-' + dia
 
@@ -13,6 +13,6 @@ export default async function proximasViagens(require, response) {
     .find({ dataIda: { $gte: new Date(stringData) } })
     .sort({ dataIda: 1 })
     .toArray()
-  //response.setHeader('Cache-Control', 's-maxage=15, stale-while-revalidate')
+  //response.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate')
   response.status(200).json(proximasViagens)
 }
