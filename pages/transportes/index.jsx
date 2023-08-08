72652a4f6d270222a97882ff3088b3b010667db3
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2023-08-08 20:31:26 -0300

    Update index.jsx
    Removendo aviso de sistema em fase de testes

--- pages/transportes/index.jsx
+++ pages/transportes/index.jsx
@@ -8,15 +8,6 @@ export default function Transportes() {
   if (session) {
     return (
       <div className="container my-12 mx-auto px-4 md:px-12">
-        <p className="aviso">
-          Atenção: Este sistema ainda está em fase de desenvolvimento. As
-          solicitações oficiais de transporte do campus Brejo Santo devem ser
-          solicitadas pelo site
-          <br />
-          <a href="https://ifeadm.16mb.com/transportes">
-            https://ifeadm.16mb.com/transportes
-          </a>
-        </p>
         <h1>Transportes do Instituto de Formação de Educadores</h1>
         <div className="button">
           <Link href="/transportes/informacoes">
