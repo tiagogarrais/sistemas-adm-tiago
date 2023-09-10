6a8ee52d06b1a1bb4bd01de5f474e58bb21caad7
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2023-09-10 00:39:04 +0000

    Update `compras`

--- pages/compras/compras.jsx
+++ pages/compras/compras.jsx
@@ -1,6 +1,23 @@
 export default function Compras() {
   return (
     <>
+      <p>
+        A responsabilidade da Administração do IFE em relação ao plano de
+        compras se resume em coletar as demandas de compras, incluí-las nos
+        respectivos sistemas e ajudar na elaboração dos documentos e andamento
+        do processo.
+      </p>
+      <p>
+        A decisão sobre os recursos que serão disponibilizados para compras é
+        uma decisão do Comitê Gestor e da Reitoria da UFCA. Demandas incluídas
+        na listagem de compras organizada pela Administração não possuem
+        garantia de compra nem de prazo para conclusão do processo.
+      </p>
+      <p>
+        A não inclusão das solicitações nesta listagem é fator impeditivo para
+        que o processo possa seguir os trâmites e caminhos normais, por isso é
+        importante que qualquer item seja adicionado nesta solicitação.
+      </p>
       <div className="button">
         <a
           href="https://docs.google.com/forms/d/e/1FAIpQLSd4d8G1TxIZ-vucdnRanQ1Mm-KH-sDjz024Tfx4a2WPf3vlOg/viewform?usp=sf_link"
