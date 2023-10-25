1d978abbc9a63a69e312abfc614425b54e252c78
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2023-10-25 15:45:04 -0300

    Update solicitaTransporteModel.js
    Retirando a obrigatoriedade do objetivo 7 após atualização do regulamento do IFE

--- models/solicitaTransporteModel.js
+++ models/solicitaTransporteModel.js
@@ -1,558 +1,553 @@
-import { Schema, model, models } from 'mongoose'
+import { Schema, model, models } from "mongoose";
 
 const solicitaTransporteSchema = new Schema({
   dataSolicitacao: {
     type: Date,
-    required: true
+    required: true,
   },
 
   objetivo: {
     type: String,
-    required: true
+    required: true,
   },
 
   nome: {
     type: String,
-    required: true
+    required: true,
   },
 
   email: {
     type: String,
-    required: true
+    required: true,
   },
 
   telefone: {
-    type: String
+    type: String,
   },
 
   veiculo: {
     type: String,
-    required: true
+    required: true,
   },
 
   uf: {
     type: String,
-    required: true
+    required: true,
   },
 
   cidade: {
     type: String,
-    required: true
+    required: true,
   },
 
   dataIda: {
     type: Date,
-    required: true
+    required: true,
   },
 
   dataRetorno: {
     type: Date,
-    required: true
+    required: true,
   },
 
   saidaCentro: {
     type: String,
-    required: true
+    required: true,
   },
 
   saidaAbaiara: {
     type: String,
-    required: true
+    required: true,
   },
 
   objetivo1: {
     type: String,
-    required: true
+    required: true,
   },
 
   objetivo2: {
     type: String,
-    required: true
+    required: true,
   },
 
   objetivo3: {
     type: String,
-    required: true
+    required: true,
   },
 
   objetivo4: {
     type: String,
-    required: true
+    required: true,
   },
 
   objetivo5: {
     type: String,
-    required: true
+    required: true,
   },
 
   objetivo6: {
     type: String,
-    required: true
-  },
-
-  objetivo7: {
-    type: String,
-    required: true
+    required: true,
   },
 
   tipo: {
     type: String,
-    required: true
+    required: true,
   },
 
   setor: {
     type: String,
-    required: true
+    required: true,
   },
 
   statusViagem: {
     type: String,
-    required: true
+    required: true,
   },
 
   passageiro1: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao1: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro2: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao2: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro3: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao3: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro4: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao4: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro5: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao5: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro6: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao6: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro7: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao7: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro8: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao8: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro9: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao9: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro10: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao10: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro11: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao11: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro12: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao12: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro13: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao13: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro14: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao14: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro15: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao15: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro16: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao16: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro17: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao17: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro18: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao18: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro19: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao19: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro20: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao20: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro21: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao21: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro22: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao22: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro23: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao23: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro24: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao24: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro25: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao25: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro26: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao26: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro27: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao27: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro28: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao28: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro29: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao29: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro30: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao30: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro31: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao31: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro32: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao32: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro33: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao33: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro34: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao34: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro35: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao35: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro36: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao36: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro37: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao37: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro38: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao38: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro39: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao39: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro40: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao40: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro41: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao41: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro42: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao42: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro43: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao43: {
     type: String,
-    required: false
+    required: false,
   },
 
   passageiro44: {
     type: String,
-    required: false
+    required: false,
   },
 
   identificacao44: {
     type: String,
-    required: false
-  }
-})
+    required: false,
+  },
+});
 
 const SolicitaTransporte =
   models.SolicitaTransporte ||
-  model('SolicitaTransporte', solicitaTransporteSchema)
+  model("SolicitaTransporte", solicitaTransporteSchema);
 
-export default SolicitaTransporte
+export default SolicitaTransporte;
