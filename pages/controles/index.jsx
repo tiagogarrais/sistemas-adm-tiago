165da9959dee9748323087e09c4b66f64d7e7ce4
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-03-20 23:26:09 -0300

    Update index.jsx
    Melhorias visuais

--- pages/controles/index.jsx
+++ pages/controles/index.jsx
@@ -1,112 +1,112 @@
-import React, { useState, useEffect } from 'react'
-import { useSession } from 'next-auth/react'
+import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 
 export default function Controles() {
-  const { data: session } = useSession()
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(true)
+  const { data: session } = useSession();
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
   const [formData, setFormData] = useState({
-    nome: '',
-    intervaloEmDias: '',
-    descricao: ''
-  })
+    nome: "",
+    intervaloEmDias: "",
+    descricao: "",
+  });
 
   useEffect(() => {
-    fetch('/api/v1/ver-controles')
-      .then(res => res.json())
-      .then(data => {
-        setData(data)
-        setLoading(false)
-      })
-  }, [])
+    fetch("/api/v1/ver-controles")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
 
   function calcularProximaExecucao(dataUltimaExecucao, frequencia) {
-    const hoje = new Date()
-    const ultimaExecucao = new Date(Date.parse(dataUltimaExecucao))
-    ultimaExecucao.setDate(ultimaExecucao.getDate() + frequencia)
-    const diffTime = ultimaExecucao.getTime() - hoje.getTime()
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
-    return diffDays
+    const hoje = new Date();
+    const ultimaExecucao = new Date(Date.parse(dataUltimaExecucao));
+    ultimaExecucao.setDate(ultimaExecucao.getDate() + frequencia);
+    const diffTime = ultimaExecucao.getTime() - hoje.getTime();
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return diffDays;
   }
 
   const confirmarExecucao = async (controleId, email, nome) => {
     const confirmado = window.confirm(
-      'Deseja confirmar a execução desta tarefa?'
-    )
+      "Deseja confirmar a execução desta tarefa?"
+    );
     if (confirmado) {
-      const response = await fetch('/api/v1/atualizar-controles', {
-        method: 'POST',
+      const response = await fetch("/api/v1/atualizar-controles", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           controleId: controleId,
           email: email,
-          nome: nome
-        })
-      })
+          nome: nome,
+        }),
+      });
       if (response.ok) {
         // Atualizar localmente os dados após a confirmação
-        const newData = data.map(controle => {
+        const newData = data.map((controle) => {
           if (controle._id === controleId) {
-            const hoje = new Date()
-            controle.dataUltimaExecucao = hoje.toISOString() // Atualiza a dataUltimaExecucao para a data atual
+            const hoje = new Date();
+            controle.dataUltimaExecucao = hoje.toISOString(); // Atualiza a dataUltimaExecucao para a data atual
           }
-          return controle
-        })
-        setData(newData)
-        alert('O controle foi atualizado!')
+          return controle;
+        });
+        setData(newData);
+        alert("O controle foi atualizado!");
       } else {
         alert(
-          'Ocorreu um erro ao confirmar a tarefa. Tente novamente mais tarde.'
-        )
+          "Ocorreu um erro ao confirmar a tarefa. Tente novamente mais tarde."
+        );
       }
     }
-  }
+  };
 
-  const handleSubmit = async event => {
-    event.preventDefault()
-    const response = await fetch('/api/v1/criar-controle', {
-      method: 'POST',
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch("/api/v1/criar-controle", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(formData)
-    })
+      body: JSON.stringify(formData),
+    });
     if (response.ok) {
       // Atualizar os dados após a criação do controle
-      const newData = await response.json()
-      setData([...data, newData])
+      const newData = await response.json();
+      setData([...data, newData]);
       // Limpar o formulário após o envio
       setFormData({
-        nome: '',
-        intervaloEmDias: '',
-        descricao: ''
-      })
+        nome: "",
+        intervaloEmDias: "",
+        descricao: "",
+      });
     } else {
       console.error(
-        'Ocorreu um erro ao criar o controle. Tente novamente mais tarde.'
-      )
+        "Ocorreu um erro ao criar o controle. Tente novamente mais tarde."
+      );
     }
     // Recarregar a página
-    window.location.reload()
-  }
+    window.location.reload();
+  };
 
-  const handleChange = event => {
-    const { name, value } = event.target
+  const handleChange = (event) => {
+    const { name, value } = event.target;
     setFormData({
       ...formData,
-      [name]: value
-    })
-  }
+      [name]: value,
+    });
+  };
 
-  if (isLoading) return <p>Buscando informações...</p>
-  if (!data) return <p>No profile data</p>
+  if (isLoading) return <p>Buscando informações...</p>;
+  if (!data) return <p>No profile data</p>;
 
   const isAdmin =
     session &&
-    (session.user.email === 'administracao.ife@ufca.edu.br' ||
-      session.user.email === 'tiago.arrais@ufca.edu.br')
+    (session.user.email === "administracao.ife@ufca.edu.br" ||
+      session.user.email === "tiago.arrais@ufca.edu.br");
 
   return (
     <>
@@ -150,6 +150,7 @@ export default function Controles() {
           {data.map((controle, index) => (
             <div key={index} className="my-1 px-1 w-full md:w-1/2 lg:w-1/3">
               <article className="min-h-300 min-h-full">
+                <small>Periodicidade: {controle.intervaloEmDias} dias</small>
                 <header className="flex items-center justify-center leading-tight p-2 md:p-4">
                   <h4>
                     <b>{controle.nome}</b>
@@ -158,26 +159,22 @@ export default function Controles() {
                 <p>{controle.descricao}</p>
 
                 <p>
-                  Este processo é executado a cada {controle.intervaloEmDias}{' '}
-                  dias
-                </p>
-                <p>
                   <b>
-                    Próxima execução em até{' '}
+                    Próxima execução em até{" "}
                     {calcularProximaExecucao(
                       controle.dataUltimaExecucao,
                       parseInt(controle.intervaloEmDias)
-                    )}{' '}
+                    )}{" "}
                     dia(s)
-                  </b>{' '}
+                  </b>{" "}
                 </p>
                 {session &&
-                  (session.user.email === 'administracao.ife@ufca.edu.br' ||
-                    session.user.email === 'tiago.arrais@ufca.edu.br' ||
-                    session.user.email === 'daniel.brandom@ufca.edu.br' ||
-                    session.user.email === 'clarisse.alves@ufca.edu.br' ||
+                  (session.user.email === "administracao.ife@ufca.edu.br" ||
+                    session.user.email === "tiago.arrais@ufca.edu.br" ||
+                    session.user.email === "daniel.brandom@ufca.edu.br" ||
+                    session.user.email === "clarisse.alves@ufca.edu.br" ||
                     session.user.email ===
-                      'alexsandra.tavares@ufca.edu.br') && (
+                      "alexsandra.tavares@ufca.edu.br") && (
                     <button
                       onClick={() =>
                         confirmarExecucao(
@@ -196,5 +193,5 @@ export default function Controles() {
         </div>
       </div>
     </>
-  )
+  );
 }
