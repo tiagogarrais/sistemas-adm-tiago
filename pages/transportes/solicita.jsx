dc2216ef4b5f6409a84f1604b3805ad71ed033d6
Tiago das Graças Arrais <37849109+tiagogarrais@users.noreply.github.com>
2024-03-27 14:13:41 -0300

    Update solicita.jsx
    Atualização para que o formulário reflita o comando de greve da UFCA

--- pages/transportes/solicita.jsx
+++ pages/transportes/solicita.jsx
@@ -1,5 +1,6 @@
-import SolicitarVeiculo from '../../components/transportes/SolicitarVeiculo'
+import SolicitarVeiculo from "../../components/transportes/SolicitarVeiculo";
+import SolicitarVeiculoGreve from "../../components/transportes/SolicitarVeiculoGreve";
 
 export default function Solicita() {
-  return <SolicitarVeiculo />
+  return <SolicitarVeiculoGreve />;
 }
